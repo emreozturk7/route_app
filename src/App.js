08c70a744f935aa8ca25f226dac1e5acb94dc7ed
emreozturk <emre_oztrk_99@hotmail.com>
2022-06-21 22:35:04 +0300

    başarısız deneme :(

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState } from "react";
 function App() {
+
+  const [routes, setRoutes] = useState([]);
+  const [count, setCount] = useState(0);
+
+  function newRoute() {
+    setRoutes([...routes, { "id": count, "nameID": "", "gpc_latID": "", "gpc_lonID": "", "SCID": "" }]);
+    setCount(count => count + 1)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setRoutes([...routes, { "id": e.index, "nameID": e.target.value, "gpc_latID": e.target.value, "gpc_lonID": e.target.value, "SCID": e.target.value }]);
+    console.log(routes);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <input type="text" placeholder="Güzergah" />
+        <br />
+        <button onClick={() => newRoute()}>Yeni Ekle</button>
+        <br />
+        <form onSubmit={handleSubmit}>
+          {
+            <div>
+              {
+                routes.map((index) => {
+                  return NewRoute(index);
+                })
+              }
+            </div>
+          }
+          <button type="submit">KAYDET</button>
+        </form>
+        <br />
+        <br />
+      </div>
     </div>
   );
+}
+
+function NewRoute(index) {
+  return (
+    <div key={index.index}>
+      <input type="text" placeholder="name" key={`name+${index.index}`} />
+      <input type="text" placeholder="gpc_lat" key={`gpc_lat+${index.index}`} />
+      <input type="text" placeholder="gpc_lon" key={`gpc_lon+${index.index}`} />
+      <input type="text" placeholder="SC" key={`SC+${index.index}`} />
+    </div>
+  )
 }
 
 export default App;
