d814bfcae2fba628fff1b65a8bb8ee61113561ef
emreozturk <emre_oztrk_99@hotmail.com>
2022-06-25 08:50:32 +0300

    Validasyon ve buton işlemi yapıldı.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ function App() {
 
   const [routes, setRoutes] = useState([]);
   const [count, setCount] = useState(0);
+  const [error, setError] = useState(false);
+  const [buttonVisible, setButtonVisible] = useState(false);
 
   function newRoute() {
+    buttonAktif();
     setCount(count => count + 1);
     setRoutes([...routes, { id: count, isim: "", gpc_lat: "", gpc_lon: "", SC: "" }]);
   }
@@ -14,55 +17,88 @@ function App() {
     let yeniListe = routes;
     yeniListe[index].isim = e.target.value;
     setRoutes(yeniListe);
+    handleError();
+    buttonAktif();
   };
 
   const handleChange2 = (index) => e => {
     let yeniListe = routes;
     yeniListe[index].gpc_lat = e.target.value;
     setRoutes(yeniListe);
+    buttonAktif();
   };
 
   const handleChange3 = (index) => e => {
     let yeniListe = routes;
     yeniListe[index].gpc_lon = e.target.value;
     setRoutes(yeniListe);
+    buttonAktif();
   };
 
   const handleChange4 = (index) => e => {
     let yeniListe = routes;
     yeniListe[index].SC = e.target.value;
     setRoutes(yeniListe);
+    buttonAktif();
   };
 
   const submit = () => {
     console.log(routes);
   }
 
+  const handleError = () => {
+    var unique = [...new Set(routes.map((route) => route.isim))];
+
+    if (unique.length < routes.length) {
+      setError(true);
+    }
+    else {
+      setError(false);
+    }
+  }
+
+  const buttonAktif = () => {
+    routes.map((route) => {
+      if (route.isim.length > 0 && route.gpc_lat.length > 0 && route.gpc_lon.length > 0 && route.SC.length > 0) {
+        setButtonVisible(true);
+      }
+      else {
+        setButtonVisible(false);
+      }
+    });
+  }
+
   return (
     <div className="App">
+      <div>
+        <input placeholder="Güzergah"></input>
+      </div>
       <button onClick={newRoute}>Yeni ekle</button>
       {
         <div>
           {
             routes.map((route) => {
-              return NewRoute(handleChange, handleChange2, handleChange3, handleChange4, route.id);
+              return NewRoute(handleChange, handleChange2, handleChange3, handleChange4, route.id, error);
             })
           }
         </div>
       }
-      <button onClick={submit}>ekle</button>
+      <button disabled={!buttonVisible} onClick={submit}>KAYDET</button>
     </div>
   );
 }
 
-function NewRoute(handleChange, handleChange2, handleChange3, handleChange4, index) {
+function NewRoute(handleChange, handleChange2, handleChange3, handleChange4, index, error) {
   return (
-    <div >
-      <div>{index}</div>
+    <div className="input-container">
       <input type="text" placeholder="name" onChange={handleChange(index)} />
       <input type="text" placeholder="gpc_lat" onChange={handleChange2(index)} />
       <input type="text" placeholder="gpc_lon" onChange={handleChange3(index)} />
       <input type="text" placeholder="SC" onChange={handleChange4(index)} />
+      {
+        error &&
+        <label>İsim değeri başka bir isim değeri ile aynı olamaz!</label>
+      }
     </div>
   )
 }
